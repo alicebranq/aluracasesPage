7f992f20e7246c99ecbc251bed0008df5fdaa98c
alicebranq <branq.alice@gmail.com>
2022-10-07 14:19:23 -0300

    first commit

---
+++ pages/fac.js
@@ -0,0 +1,12 @@
+import NextLink from "../src/components/Link"
+
+export default function FAQ() {
+
+    return (
+        <div>
+            <NextLink href="/">
+                <a> Home </a>
+            </NextLink>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import NextLink from "../src/components/Link"
+
+export default function HomePage() {
+
+    return (
+        <div>
+            <h1> Alura Cases - Home Page </h1>
+            <NextLink href="/fac">
+                Ir para FAQ
+            </NextLink>
+        </div>
+    )
+}
