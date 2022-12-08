fad11b16b1ac328d7bf634e83b4a2ebf4b33ee8d
alicebranq <branq.alice@gmail.com>
2022-12-08 17:21:42 -0300

    add GlobalStyle e CSS-in-JS base

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
+import React from "react"
+import { Children } from "react"
 import Link from "../src/components/Link"
+import GlobalStyle from "../src/theme/GlobalStyle";
+
+
+function Title({children, as}) {
+    const Tag = as;
+    return (
+        <React.Fragment>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>{`
+                ${Tag} {
+                    color: red;
+                }
+            `} </style>
+        </React.Fragment>
+    )
+}
 
 export default function HomePage() {
 
     return (
         <div>
+            <GlobalStyle />
             <h1> Alura Cases - Home Page </h1>
             <Link href="/fac">
                 <a> Ir para FAQ </a> 
